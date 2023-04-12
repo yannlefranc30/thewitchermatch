083b9bf10db23380a90093074f97d691e1e17e17
yann <yannlefranc330@gmail.com>
2023-04-12 13:48:16 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    // Cette methode permet de trouver la valeur la plus élver ; Math min permet de trouver la valeur la moins éléver.
+    public static void main(String[] args) {
+        System.out.println(Math.max(8, 15));
+    }
+}
